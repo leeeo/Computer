50abbafc01d710555000c18de08d2d31bb0f7c0d
이어진 <eojin@mail.com>
2015-05-28 16:50:43 +0900

    A Byte of Python 예제 정리2

---
+++ python/function_return/function_return.py
@@ -0,0 +1,13 @@
+def maximum(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return 'The numbers are equal'
+    else:
+        return y
+
+print maximum(2, 3)
+
+#pass 문 - 아무 기능이 없는 구문입니다. 이것은 빈 블록을 지정해 줄 대 사용됩니다.
+#def some_function():
+#    pass
